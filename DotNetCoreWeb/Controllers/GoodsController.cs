fe54e8c146423d5c5bdf1b06e71034477ec5b029
playitouch@163.com <playitouch@163.com>
2019-07-03 23:56:26 +0800

    提交

--- DotNetCoreWeb/Controllers/GoodsController.cs
+++ DotNetCoreWeb/Controllers/GoodsController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetCoreWeb.DTO;
 using Microsoft.AspNetCore.Mvc;
 using MyProject.EntityFrameworkCore;
 
@@ -9,12 +11,29 @@ namespace DotNetCoreWeb.Controllers
 {
     public class GoodsController : Controller
     {
+        private Hashtable ht = new Hashtable();
+
         public IActionResult Index()
         {
             using (var db = new EfDbContext())
             {
+                var output = new List<GoodsOutput>();
                 var goods = db.Goods.ToList();
-                return View(goods);
+                foreach (var item in goods)
+                {
+                    var dto = new GoodsOutput();
+                    dto.Id = item.Id;
+                    dto.Name = item.Name;
+                    //ht.Add("修改", "javascript:void(0)");
+                    ht.Add($"javascript:do_miaosha({item.Id})", "秒杀");
+
+                    foreach (var key in ht.Keys)
+                    {
+                        dto.Operate = $"<a href=\"{key}\">{ht[key]}</a>";
+                    }
+                    output.Add(dto);
+                }
+                return View(output);
             }
         }
 
---
+++ DotNetCoreWeb/DTO/GoodsOutput.cs
@@ -0,0 +1,16 @@
+﻿using MyProject.Core;
+using System.Collections;
+
+namespace DotNetCoreWeb.DTO
+{
+    public class GoodsOutput:BaseEntity<long>
+    {
+        public string Name { get; set; }
+
+        public string Operate { get; set; }
+
+        public GoodsOutput()
+        {
+        }
+    }
+}
