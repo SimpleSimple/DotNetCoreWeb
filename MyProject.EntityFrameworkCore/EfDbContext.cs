90849d4eac4c168e74884644a5b95639e249634f
wanghewen <wanghewen@skong.com>
2019-07-11 18:05:12 +0800

    提交

--- MyProject.EntityFrameworkCore/EfDbContext.cs
+++ MyProject.EntityFrameworkCore/EfDbContext.cs
@@ -10,9 +10,9 @@ namespace MyProject.EntityFrameworkCore
     public class EfDbContext : DbContext
     {
         // 不使用构造函数方式，使用 OnConfiguring 方法
-        //public EfDbContext(DbContextOptions<EfDbContext> options)
-        //    : base(options)
-        //{ }
+        public EfDbContext(DbContextOptions<EfDbContext> options) : base(options)
+        {
+        }
 
         public virtual DbSet<Order> Orders { get; set; }
 
@@ -32,13 +32,13 @@ namespace MyProject.EntityFrameworkCore
             //    .AddJsonFile("appsettings.json")
             //    .Build();
 
-            // 注意：这里获取appsettings.json文件要复制到本地
-            var config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .Build();
-            // 连接SqlServer
-            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            //// 注意：这里获取appsettings.json文件要复制到本地
+            ////var config = new ConfigurationBuilder()
+            ////    .SetBasePath(Directory.GetCurrentDirectory())
+            ////    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            ////    .Build();
+            //// 连接SqlServer
+            //optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
         }
     }
 }
